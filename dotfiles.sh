e452087ce43f020d09636948cf1aedf94c3283f7
Steven BOEHM <boehm_s@etna-alternance.net>
2017-10-24 18:44:19 +0200

    my emacs conf as readme

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,6 +5,7 @@ ln -s .emacs.d/conf/dotemacs.el .emacs
 
 cp -r .emacs.d dotfiles/
 cp .emacs.d/conf/emacs-config.org dotfiles/emacs.org
+cp .emacs.d/conf/emacs-config.org dotfiles/README.org
 cp .bashrc dotfiles/
 cp dotfiles.sh dotfiles/
 cp .tern-config dotfiles/
